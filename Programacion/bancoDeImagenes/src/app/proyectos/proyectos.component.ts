e9532acb1cf25d612049b3b69b66789e9312833a
Susana Felix Orozco <susiibones@gmail.com>
2021-05-25 13:33:32 -0500

    Interfaz de proyectos responsiva

--- Programacion/bancoDeImagenes/src/app/proyectos/proyectos.component.ts
+++ Programacion/bancoDeImagenes/src/app/proyectos/proyectos.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyectos.component.css']
 })
 export class ProyectosComponent implements OnInit {
+mostrarInfo=false;
 
   constructor() { }
 
